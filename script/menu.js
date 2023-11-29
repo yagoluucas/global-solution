6990f4d4501ade6439cbe8f7ee8d9f06af370afb
yagoluucas <yago543@gmail.com>
2023-11-29 09:46:32 -0300

    adicionda menu responsivo

---
+++ script/menu.js
@@ -0,0 +1,19 @@
+window.addEventListener('DOMContentLoaded', () => {
+    const btnMenu = document.querySelector('.js-icone-menu')
+    const links = document.querySelector('header nav ul')
+    let eventos = ['click', 'touchend']
+    eventos.forEach((e) => {
+        btnMenu.addEventListener(e, () => {
+            links.classList.toggle('mostrar')
+            links.classList.toggle('ocultar')
+            if(links.classList.contains('mostrar')){
+                btnMenu.setAttribute('src', '../image/fechar.png')
+                btnMenu.setAttribute('alt', 'icone para fehcar o menu')
+            } else {
+                btnMenu.setAttribute('src', '../image/hamburger.png')
+                btnMenu.setAttribute('alt', 'icone para abrir o menu')
+            }
+        })
+    })
+    
+})
