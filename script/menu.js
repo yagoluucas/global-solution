71de1cbcefbeff905c8efe7bb66a729efcddc516
yagoluucas <yago543@gmail.com>
2023-12-22 22:24:21 -0300

    arrumado erro de portugues no icone de menu

--- script/menu.js
+++ script/menu.js
@@ -8,7 +8,7 @@ window.addEventListener('DOMContentLoaded', () => {
             links.classList.toggle('ocultar')
             if(links.classList.contains('mostrar')){
                 btnMenu.setAttribute('src', '../image/fechar.png')
-                btnMenu.setAttribute('alt', 'icone para fehcar o menu')
+                btnMenu.setAttribute('alt', 'icone para fechar o menu')
             } else {
                 btnMenu.setAttribute('src', '../image/hamburger.png')
                 btnMenu.setAttribute('alt', 'icone para abrir o menu')
